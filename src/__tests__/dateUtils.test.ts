8864429039f7ae58ab7d427b93ec7bcf73d2ff9c
viktorhollanders <viktorhollanders@hey.com>
2026-02-04 17:05:28 +0000

    Add getHoliday test adn remove todo from it.

--- src/__tests__/dateUtils.test.ts
+++ src/__tests__/dateUtils.test.ts
@@ -1,5 +1,11 @@
 import { describe, it, expect, vi } from "vitest";
-import { getCurrentYear, add, isWithinRange, isDateBefore } from "../dateUtils";
+import {
+  getCurrentYear,
+  add,
+  isWithinRange,
+  isDateBefore,
+  getHolidays,
+} from "../dateUtils";
 import moment from "moment";
 import { isSameDay } from "date-fns/fp";
 
@@ -7,7 +13,7 @@ describe("Date Utils", () => {
   // Add your tests here
 
   describe("test getCurrentYear", () => {
-    it.todo("should return current year", () => {
+    it("should return current year", () => {
       const year_1995 = { year: vi.fn().mockReturnValue(1995) };
       vi.mocked(moment).mockReturnValue(year_1995 as any);
       expect(getCurrentYear()).toBe(1995);
@@ -15,36 +21,36 @@ describe("Date Utils", () => {
   });
 
   describe("test add", () => {
-    it.todo("should add time to date", () => {
+    it("should add time to date", () => {
       const start_date = new Date(1995, 3, 2, 10, 30);
       const end_date = new Date(1995, 3, 7, 10, 30);
       expect(add(start_date, 5)).toEqual(end_date);
     });
     // Invalid date inputs
-    it.todo("should throw error for null", () => {
+    it("should throw error for null", () => {
       expect(add(null, 5)).toThrowError("Invalid date provided");
     });
-    it.todo("should throw error for undefined", () => {
+    it("should throw error for undefined", () => {
       expect(add(undefined, 5)).toThrowError("Invalid date provided");
     });
-    it.todo("should throw error for wrong data type", () => {
+    it("should throw error for wrong data type", () => {
       expect(add("1995, 3, 2, 10, 30", 5)).toThrowError(
         "Invalid date provided",
       );
     });
-    it.todo("should throw error for invalid Date object", () => {
+    it("should throw error for invalid Date object", () => {
       const invalidDate = new Date("hello");
       expect(add(invalidDate, 5)).toThrowError("Invalid date provided");
     });
     // invalid amount input
-    it.todo("should throw an error if the amount is not a number", () => {
+    it("should throw an error if the amount is not a number", () => {
       const date = new Date(1995, 3, 2, 10, 30);
       expect(add(date, "5")).toThrowError("Invalid amount provided");
     });
   });
 
   describe("test isWithinRange", () => {
-    it.todo("should returns date if it is within the given range", () => {
+    it("should returns date if it is within the given range", () => {
       const from = new Date(1995, 3, 2);
       const to = new Date(1995, 3, 7);
       const date = new Date(1995, 3, 5);
@@ -52,34 +58,31 @@ describe("Date Utils", () => {
       expect(isWithinRange(date, from, to)).toBe(true);
     });
     // Invalid range
-    it.todo("should return false when date is not in range", () => {
+    it("should return false when date is not in range", () => {
       const from = new Date(1995, 3, 2);
       const to = new Date(1995, 3, 7);
       const date = new Date(1995, 3, 10);
 
       expect(() => isWithinRange(date, from, to)).toBe(false);
     });
-    it.todo(
-      "should throw and error when (from  date) is grater than (to date)",
-      () => {
-        const from = new Date(1995, 3, 7);
-        const to = new Date(1995, 3, 2);
-        const date = new Date(1995, 3, 10);
+    it("should throw and error when (from  date) is grater than (to date)", () => {
+      const from = new Date(1995, 3, 7);
+      const to = new Date(1995, 3, 2);
+      const date = new Date(1995, 3, 10);
 
-        expect(() => isWithinRange(date, from, to)).toThrowError(
-          "Invalid range: from date must be before to date",
-        );
-      },
-    );
+      expect(() => isWithinRange(date, from, to)).toThrowError(
+        "Invalid range: from date must be before to date",
+      );
+    });
   });
 
   describe("test isDateBefore", () => {
-    it.todo("should return true if date is before compareDate", () => {
+    it("should return true if date is before compareDate", () => {
       const date = new Date(1918, 4, 21);
       const compareDate = new Date(1918, 8, 9);
       expect(isDateBefore(date, compareDate)).toBe(true);
     });
-    it.todo("should return false if date is after compareDate", () => {
+    it("should return false if date is after compareDate", () => {
       const date = new Date(1918, 4, 21);
       const compareDate = new Date(1918, 4, 20);
       expect(isDateBefore(date, compareDate)).toBe(false);
@@ -87,18 +90,27 @@ describe("Date Utils", () => {
   });
 
   describe("test isSameDay", () => {
-    it.todo("should return true if the date match compareDate", () => {
+    it("should return true if the date match compareDate", () => {
       const date = new Date(1918, 4, 21);
       const compareDate = new Date(1918, 4, 21);
       expect(isSameDay(date, compareDate)).toBe(true);
     });
-    it.todo(
-      "should return false if the date dose not match compareDate",
-      () => {
-        const date = new Date(1918, 4, 21);
-        const compareDate = new Date(1918, 4, 19);
-        expect(isSameDay(date, compareDate)).toBe(false);
-      },
-    );
+    it("should return false if the date dose not match compareDate", () => {
+      const date = new Date(1918, 4, 21);
+      const compareDate = new Date(1918, 4, 19);
+      expect(isSameDay(date, compareDate)).toBe(false);
+    });
+  });
+
+  describe("test getHolidays", () => {
+    it("should return the given holidays for the inputted year", async () => {
+      const year = 1992;
+      const data = await getHolidays(year);
+      expect(data).toEqual([
+        new Date(1992, 0, 1), // New Year's Day
+        new Date(1992, 11, 25), // Christmas
+        new Date(1992, 11, 31), // New Year's Eve
+      ]);
+    });
   });
 });
